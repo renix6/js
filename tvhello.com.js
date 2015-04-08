a89441e8442d1a08d0d04bf1f0ec99c49b9a0504
renix6 <renix6@gmail.com>
2015-04-07 23:23:17 -0600

    test

--- tvhello.com.js
+++ tvhello.com.js
@@ -1,21 +1,16 @@
 addKiller("tvhello", {
 	"canKill": function(data) {
-				return true;
+	    return data.src.indexOf('tvhello.com') !== -1;
 	},
 
 	"process": function(data, callback) {
 		var flashvars = parseFlashVariables(data.params.flashvars);
-		var match = flashvars.vodUrl.match(/^http:\/\/(.+)\.mp4$/i);
 		if(match) {
-			var start_time = flashvars.startTime;
-			var end_time = flashvars.endTime;
-			var img_url = flashvars.imgUrl;
-			//var url = "http://"+ match[1] +".mp4/playlist.m3u8?wowzaplaystart="+ start_time +"&wowzaplayduration="+ (end_time - start_time);
 			callback({
 				"playlist": [{
 					"poster": img_url,
 					"sources": [{
-						"url": url,
+				        "url" : decodeURIComponent(params.file),
 						"format": "MP4",
 						"isNative": true
 					}]
